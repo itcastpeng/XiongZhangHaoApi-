12bc4a0b0d006af13bae8e3f0a62a0a8bd3d1664
zhaoxinpeng <15531506906@sina.cn>
2018-11-09 16:16:27 +0800

    优化

--- xiongzhanghao/views_dir/user.py
+++ xiongzhanghao/views_dir/user.py
@@ -285,9 +285,10 @@ def login_website_backstage(user_id, domain, home_path, userid, pwd, flag_num):
 def objLogin(obj):
     website_backstage_url = obj.website_backstage_url
     home_path = website_backstage_url.split('/')[-1]
-    domain = website_backstage_url.split(website_backstage_url.split('/')[-1])[0]
     if home_path[-1] == '/':
         domain = website_backstage_url.split(website_backstage_url.split('/')[0].split('/')[-1])[0]
+    else:
+        domain = website_backstage_url.split(website_backstage_url.split('/')[-1])[0]
     userid = obj.website_backstage_username
     pwd = obj.website_backstage_password
     flag_num = 1# 判断登录几次 大于五次不登录
