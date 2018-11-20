2cb9fe067723854260d495003c23c1590d93a0e4
zhaoxinpeng <15531506906@sina.cn>
2018-11-20 10:00:18 +0800

    优化

--- XiongZhangHaoApi_celery/tasks.py
+++ XiongZhangHaoApi_celery/tasks.py
@@ -12,9 +12,9 @@ import requests, datetime, os, sys
 def celeryGetDebugUser(userLoginId=None):
     # url = '127.0.0.1:8003/getTheDebugUser'
     if userLoginId:
-        url = 'http://xiongzhanghao.zhugeyingxiao.com:8003/getTheDebugUser?userLoginId={}'.format(userLoginId)
+        url = 'http://xiongzhanghao.zhugeyingxiao.com:8003/userGetCookieOper/getTheDebugUser?userLoginId={}'.format(userLoginId)
     else:
-        url = 'http://xiongzhanghao.zhugeyingxiao.com:8003/getTheDebugUser'
+        url = 'http://xiongzhanghao.zhugeyingxiao.com:8003/userGetCookieOper/getTheDebugUser'
     print('url -->', url)
     requests.get(url)
 
@@ -23,13 +23,13 @@ def celeryGetDebugUser(userLoginId=None):
 @app.task
 def celeryPublishedArticles():
     # url = 'http://127.0.0.1:8003/celeryTimed'
-    urlAudit = 'http://xiongzhanghao.zhugeyingxiao.com:8003/celeryTimed'  # 查询审核
-    url = 'http://xiongzhanghao.zhugeyingxiao.com:8003/script_oper'
+    urlAudit = 'http://xiongzhanghao.zhugeyingxiao.com:8003/script_oper/celeryTimedRefreshAudit'  # 查询审核
+    url = 'http://xiongzhanghao.zhugeyingxiao.com:8003/script_oper/sendArticle'
     requests.get(url)
     requests.get(urlAudit)
 
 # 提交到熊掌号
 @app.task
 def celerySubmitXiongZhangHao():
-    url = 'http://xiongzhanghao.zhugeyingxiao.com:8003/submitXiongZhangHao'
+    url = 'http://xiongzhanghao.zhugeyingxiao.com:8003/script_oper/submitXiongZhangHao'
     requests.get(url)
