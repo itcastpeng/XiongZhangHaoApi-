be128086d3b1f87918ef027336b26ed7c4561c0d
zhaoxinpeng <15531506906@sina.cn>
2018-11-21 14:39:04 +0800

    优化

--- XiongZhangHaoApi_celery/tasks.py
+++ XiongZhangHaoApi_celery/tasks.py
@@ -9,23 +9,14 @@ import requests, datetime, os, sys
 
 # 生成二级域名
 @app.task
-def celeryGetDebugUser():
+def specialUserGenerateThePage():
     url = 'http://xiongzhanghao.zhugeyingxiao.com:8003/api/specialUserGenerateThePage'
     print('url -->', url)
     requests.get(url)
 
 
-# 定时发布文章(1分钟一次)
-# @app.task
-# def celeryPublishedArticles():
-#     # url = 'http://127.0.0.1:8003/celeryTimed'
-#     urlAudit = 'http://xiongzhanghao.zhugeyingxiao.com:8003/articleScriptOper/celeryTimedRefreshAudit'  # 查询审核
-#     url = 'http://xiongzhanghao.zhugeyingxiao.com:8003/articleScriptOper/sendArticle'
-#     requests.get(url)
-#     requests.get(urlAudit)
-
 # 提交到熊掌号
-# @app.task
-# def celerySubmitXiongZhangHao():
-#     url = 'http://xiongzhanghao.zhugeyingxiao.com:8003/articleScriptOper/submitXiongZhangHao'
-#     requests.get(url)
+@app.task
+def celerySubmitXiongZhangHao():
+    url = 'http://xiongzhanghao.zhugeyingxiao.com:8003/api/script_oper/articleScriptOper/submitXiongZhangHao'
+    requests.get(url)
